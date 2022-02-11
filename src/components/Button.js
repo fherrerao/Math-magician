78c97f255fa6c36f66629f17d57f4ef0b85f2b30
Fernando Herrera <fherrera0206@gmail.com>
2022-02-10 23:32:03 -0500

    Replace classes by hooks

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Button extends React.Component {
-  render() {
-    const { buttonName, buttonClasses, btnClick } = this.props;
-    return (
-      <button type="button" className={buttonClasses} onClick={btnClick}>{buttonName}</button>
-    );
-  }
-}
+const Button = ({ buttonName, buttonClasses, btnClick }) => {
+  return (
+    <button type="button" className={buttonClasses} onClick={btnClick}>{buttonName}</button>
+  );
+};
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,54 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import Button from './Button';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.btnOnClick = this.btnOnClick.bind(this);
+const Calculator = () => {
+
+  const [calc, setCalc] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  const btnClick = (event) => {
+    setCalc(calculate(calc, event.target.textContent));
   }
 
-  btnOnClick = (event) => {
-    this.setState((state) => calculate(state, event.target.textContent));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="container-calculator container">
-        <div className="display-result">
-          {total}
-          {operation}
-          {next}
-        </div>
-
-        <Button btnClick={this.btnOnClick} buttonName="AC" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="+/-" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="%" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="÷" buttonClasses="btn-orange" />
-        <Button btnClick={this.btnOnClick} buttonName="7" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="8" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="9" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="x" buttonClasses="btn-orange" />
-        <Button btnClick={this.btnOnClick} buttonName="4" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="5" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="6" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="-" buttonClasses="btn-orange" />
-        <Button btnClick={this.btnOnClick} buttonName="1" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="2" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="3" buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="+" buttonClasses="btn-orange" />
-        <Button btnClick={this.btnOnClick} buttonName="0" buttonClasses="btn-gray bottom-0" />
-        <Button btnClick={this.btnOnClick} buttonName="." buttonClasses="btn-gray" />
-        <Button btnClick={this.btnOnClick} buttonName="=" buttonClasses="btn-orange" />
+  return (
+    <div className="container-calculator container">
+      <div className="display-result">
+        {calc.total}
+        {calc.operation}
+        {calc.next}
       </div>
-    );
-  }
-}
+
+      <Button btnClick={btnClick} buttonName="AC" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="+/-" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="%" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="÷" buttonClasses="btn-orange" />
+      <Button btnClick={btnClick} buttonName="7" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="8" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="9" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="x" buttonClasses="btn-orange" />
+      <Button btnClick={btnClick} buttonName="4" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="5" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="6" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="-" buttonClasses="btn-orange" />
+      <Button btnClick={btnClick} buttonName="1" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="2" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="3" buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="+" buttonClasses="btn-orange" />
+      <Button btnClick={btnClick} buttonName="0" buttonClasses="btn-gray bottom-0" />
+      <Button btnClick={btnClick} buttonName="." buttonClasses="btn-gray" />
+      <Button btnClick={btnClick} buttonName="=" buttonClasses="btn-orange" />
+    </div>
+  );
+
+};
 
 export default Calculator;
