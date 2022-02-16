f55a6ba565e2b47f8478588861713d2e590e4499
Fernando Herrera <fherrera0206@gmail.com>
2022-02-16 11:00:03 -0500

    Create quote.js file

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,3 +1,14 @@
-const Quote = () => {
+const quot = 'Mathematicians have tried in vain to this day to discover some order in the sequence of prime numbers, and we have reason to believe that it is a mystery into which the human mind will never penetrate.';
+const author = '–  Leonhard Euler.';
 
-}
+const Quote = () => (
+  <div>
+    <blockquote>
+      <h2>
+        {quot}
+        <span>{author}</span>
+      </h2>
+    </blockquote>
+  </div>
+);
+export default Quote;
