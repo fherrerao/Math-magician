431c13378866d88925010fdb4f30787b978b52b4
Fernando Herrera <fherrera0206@gmail.com>
2022-02-16 12:03:45 -0500

    Add styles to calculator and quotes pages

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,32 +14,36 @@ const Calculator = () => {
   };
 
   return (
-    <div className="container-calculator container">
-      <div className="display-result">
-        {calc.total}
-        {calc.operation}
-        {calc.next}
+    <div className="calculator container">
+      <h1 className="container">Let&apos;s do some maths:</h1>
+      <div className="container-calculator container">
+        <div className="display-result">
+          {calc.total}
+          {calc.operation}
+          {calc.next}
+        </div>
+
+        <Button btnClick={btnClick} buttonName="AC" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="+/-" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="%" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="÷" buttonClasses="btn-orange" />
+        <Button btnClick={btnClick} buttonName="7" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="8" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="9" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="x" buttonClasses="btn-orange" />
+        <Button btnClick={btnClick} buttonName="4" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="5" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="6" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="-" buttonClasses="btn-orange" />
+        <Button btnClick={btnClick} buttonName="1" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="2" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="3" buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="+" buttonClasses="btn-orange" />
+        <Button btnClick={btnClick} buttonName="0" buttonClasses="btn-gray bottom-0" />
+        <Button btnClick={btnClick} buttonName="." buttonClasses="btn-gray" />
+        <Button btnClick={btnClick} buttonName="=" buttonClasses="btn-orange" />
       </div>
 
-      <Button btnClick={btnClick} buttonName="AC" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="+/-" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="%" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="÷" buttonClasses="btn-orange" />
-      <Button btnClick={btnClick} buttonName="7" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="8" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="9" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="x" buttonClasses="btn-orange" />
-      <Button btnClick={btnClick} buttonName="4" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="5" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="6" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="-" buttonClasses="btn-orange" />
-      <Button btnClick={btnClick} buttonName="1" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="2" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="3" buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="+" buttonClasses="btn-orange" />
-      <Button btnClick={btnClick} buttonName="0" buttonClasses="btn-gray bottom-0" />
-      <Button btnClick={btnClick} buttonName="." buttonClasses="btn-gray" />
-      <Button btnClick={btnClick} buttonName="=" buttonClasses="btn-orange" />
     </div>
   );
 };
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,15 @@
+import './quote.css';
+
 const quot = 'Mathematicians have tried in vain to this day to discover some order in the sequence of prime numbers, and we have reason to believe that it is a mystery into which the human mind will never penetrate.';
 const author = '–  Leonhard Euler.';
 
 const Quote = () => (
-  <div>
-    <blockquote>
-      <h2>
+  <div className="block-container">
+    <blockquote className="block">
+      <h3>
         {quot}
-        <span>{author}</span>
-      </h2>
+      </h3>
+      <h2>{author}</h2>
     </blockquote>
   </div>
 );
